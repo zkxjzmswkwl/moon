d388d881e879fe8ae1e02453ca742c7e8d6fbd9d
zkxjzmswkwl <smithcarterfreelance@gmail.com>
2020-11-03 18:25:01 -0700

    Base interface

---
+++ moon.py
@@ -0,0 +1,17 @@
+import sys
+import spotipy
+import spotipy.util as util
+import dearpygui.core as core
+import dearpygui.simple as simple
+
+
+def login(sender, data):
+    pass
+
+with simple.window('Moon Overwatch Leveling Bot'):
+    core.add_input_text('Account Email', default_value='smurf@cock.li')
+    core.add_input_text('Account Password', default_value='mature_password!')
+    core.add_button('Login', callback=login)
+
+core.set_theme('Red')
+core.start_dearpygui(primary_window='Moon Overwatch Leveling Bot')
