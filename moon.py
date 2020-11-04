602b340bb14ff99a49646e3d647385313fd55bcb
zkxjzmswkwl <smithcarterfreelance@gmail.com>
2020-11-03 19:11:57 -0700

    Modified some widget dimensions, added some spacing, changed font, added separators and padding

--- moon.py
+++ moon.py
@@ -14,12 +14,34 @@ def multiple_accounts(sender, data):
             default_value='accs_to_level.txt',
             before='Login')
 
-
 with simple.window('Moon Overwatch Leveling Bot'):
-    core.add_input_text('Account Email', default_value='smurf@cock.li')
-    core.add_input_text('Account Password', default_value='mature_password!')
-    core.add_checkbox('Multiple Accounts', default_value=False, callback=multiple_accounts)
-    core.add_button('Login', callback=login)
+    core.set_style_item_spacing(15, 15)
+    core.set_style_frame_padding(5, 5)
+#    core.set_global_font_scale(1.5)
+    core.add_additional_font('Roboto-Black.ttf', 14)
+#    core.add_additional_font('Yellowtail-Regular.ttf', 16)
+
+    core.add_text('Moon')
+    core.add_separator()
+    
+    core.add_input_text(
+            'Account Email',
+            default_value='smurf@cock.li',
+            width=200)
+
+    core.add_input_text(
+            'Account Password',
+            default_value='mature_password!',
+            width=200)
+    
+    core.add_checkbox(
+            'Multiple Accounts',
+            default_value=False,
+            callback=multiple_accounts)
+    
+    core.add_same_line()
+    core.add_button('Login', callback=login, width=85)
+    core.add_separator()
 
 core.set_theme('Red')
 core.start_dearpygui(primary_window='Moon Overwatch Leveling Bot')
